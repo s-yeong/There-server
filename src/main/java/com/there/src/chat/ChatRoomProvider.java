82e7e92773ce72dd9c21abfc61e29aeb839161de
ParkRootSeok <g_e_un@naver.com>
2022-08-11 20:21:40 +0900

    REFACTOR: 채팅방 리스트 조회 API 오류 수정

--- src/main/java/com/there/src/chat/ChatRoomProvider.java
+++ src/main/java/com/there/src/chat/ChatRoomProvider.java
@@ -3,6 +3,7 @@ package com.there.src.chat;
 import com.there.config.BaseException;
 import com.there.config.BaseResponseStatus;
 import com.there.src.chat.model.GetRoomInfoRes;
+import com.there.src.chat.model.GetRoomListRes;
 import com.there.src.chat.model.GetUserInfoRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,9 +30,9 @@ public class ChatRoomProvider {
     /**
      * 채팅방 목록 조회
      */
-    public List<GetRoomInfoRes> retrieveChatRoom(int userIdx) {
+    public List<GetRoomListRes> retrieveChatRoom(int userIdx) {
 
-        List<GetRoomInfoRes> getRoomInfoList = chatRoomDao.selectChatRoomList(userIdx);
+        List<GetRoomListRes> getRoomInfoList = chatRoomDao.selectChatRoomList(userIdx);
 
         // 읽은 메시지로 상태 변경 (단, 읽지 않은 메시지가 있을 때만)
         if (chatContentDao.selectUnCheckCount(userIdx) > 0) chatContentDao.checkChatContent(userIdx);
