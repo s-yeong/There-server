294fc63e8f0eea910ee72a8391afb6c84a3f93b7
ParkRootSeok <g_e_un@naver.com>
2022-08-24 15:39:11 +0900

    REFACTOR: POST 조회 API 쿼리 수정

--- src/main/java/com/there/src/post/PostDao.java
+++ src/main/java/com/there/src/post/PostDao.java
@@ -24,7 +24,7 @@ public class PostDao {
     public GetPostsRes selectPosts(int postIdx) {
 
         String selectPostsQuery = "select u.profileImgUrl as profileImgUrl, u.nickName as nickName, p.postIdx as postIdx, imgUrl, content,\n" +
-                "       if(count(likesIdx) is null, 0, count(likesIdx)) as likeCount\n" +
+                "       if(count(likeIdx) is null, 0, count(likeIdx)) as likeCount\n" +
                 "from Post as p\n" +
                 "    join User as u on u.userIdx = p.userIdx\n" +
                 "    left join postLike as ps on ps.postIdx = p.postIdx\n" +
